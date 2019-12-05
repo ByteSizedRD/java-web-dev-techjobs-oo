92a65febc1892f3b22b76586b5c7cd7f1dd30eae
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2019-12-04 20:47:39 -0600

    Redoing setters/getters in support files on desktop d/t not saving on laptop

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -37,6 +37,17 @@ public class CoreCompetency {
         return Objects.hash(id);
     }
 
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
+//generated getter for id
+    public int getId() {
+        return id;
+    }
+
+//generated getter/setter for value
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -12,8 +12,7 @@ public class Location {
         nextId++;
     }
 
-    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
-    //  constructor should also call the empty constructor in order to initialize the 'id' field.
+    //added constructor with parameter String value , calls empty constructor (this();) and initializes value
 
     public Location(String value) {
         this();
