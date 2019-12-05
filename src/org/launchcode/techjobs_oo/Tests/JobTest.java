dc5c9b3a149ec0e45f91a6e3d2237b6cac1c5fe5
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2019-12-05 01:36:11 -0600

    Test the Full Constructor completed

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,8 +2,9 @@ package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 
@@ -12,6 +13,7 @@ public class JobTest {
     //creating fields for testing
     Job new_job1;
     Job new_job2;
+    Job new_job3;
 
     //using @Before to set Job objects for testing
     @Before
@@ -25,6 +27,21 @@ public class JobTest {
     public void testSettingJobId() {
         assertTrue(!new_job1.equals(new_job2));
         assertTrue(new_job1.getId() + 1 == new_job2.getId());
+    }
+
+    // each Job object should contain 6 fields & proper data types
+    //int id, String name, Employer employer, Location location, PostiionType postiontype, CoreCompetency corecompetency
+
+    @Test
+    public void testJobConstructorSetAllFields() {
+        new_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(new_job3 instanceof Job);
+        assertEquals(3, new_job3.getId(), .001);
+        assertTrue((new_job3.getName() == "Product tester"));
+        assertTrue((new_job3.getEmployer().getValue() == "ACME"));
+        assertTrue((new_job3.getLocation().getValue() =="Desert"));
+        assertTrue((new_job3.getPositionType().getValue() == "Quality control"));
+        assertTrue((new_job3.getCoreCompetency().getValue() == "Persistence"));
     }
 
 
